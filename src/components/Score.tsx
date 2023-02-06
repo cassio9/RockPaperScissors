94de7f731446cac75f02c66808446a6e5fe94499
Cassio Soares <cassiosoares9@hotmail.com>
2023-02-06 15:27:27 +0900

    add change game

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -6,7 +6,13 @@ const Score = () => {
 	const score = useGameStore((state) => state.score);
 	const gamePlaying = useGameStore((state) => state.gamePlaying);
 	const setGamePlaying = useGameStore((state) => state.setGamePlaying);
-	const setScore = useGameStore((state) => state.setScore);
+	const setScoreZero = useGameStore((state) => state.setScoreZero);
+	const setRestart = useGameStore((state) => state.setRestart);
+
+	const setChangeGame = () => {
+		setGamePlaying("");
+		setRestart();
+	};
 
 	return (
 		<header className="flex justify-between items-center border-4 border-HeaderOutline rounded-lg w-full max-w-md py-2 lg:py-4 px-6 lg:max-w-2xl z-10 bg-RadialGradient">
@@ -17,12 +23,12 @@ const Score = () => {
 			/>
 			<div className="flex flex-col gap-2 w-16 md:w-auto">
 				<button
-					onClick={() => setScore()}
+					onClick={() => setScoreZero()}
 					className="text-HeaderOutline hover:text-white hover:border-white text-sm border-2 rounded-md md:py-2 md:px-4 border-HeaderOutline">
 					Reset Score
 				</button>
 				<button
-					onClick={() => setGamePlaying("")}
+					onClick={setChangeGame}
 					className="text-HeaderOutline hover:text-white hover:border-white text-sm border-2 rounded-md md:py-2 md:px-4 border-HeaderOutline">
 					Change Game
 				</button>
